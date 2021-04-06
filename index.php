9f6b4ff3770e4eb56188fa94de132836367711cc
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-04-06 16:11:41 +0200

    fixed mistake

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     class BytesToBits {
         protected $token;
 
-        function __constructor($token) {
+        function __construct($token) {
             $this->curl = curl_init();
             $this->token = $token;
             $this->base_url = "https://api.bytestobits.dev/";
             $header_obj->Authorization = $token;
             $this->header = json_encode($header_obj);
         }
-        function __destructor() {
+        function __destruct() {
             curl_close($this->curl);
         }
     }
