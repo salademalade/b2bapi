4c358d147f0765a459b54d09f02e8be7894a0cff
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-04-06 16:29:51 +0200

    added text endpoint method

--- index.php
+++ index.php
@@ -7,11 +7,18 @@
             $this->curl = curl_init();
             $this->token = $token;
             $this->base_url = "https://api.bytestobits.dev/";
-            $header_obj->Authorization = $token;
-            $this->header = json_encode($header_obj);
+            $this->headers = array(
+                "Authorization: ".$token
+            );
         }
         function __destruct() {
             curl_close($this->curl);
+        }
+        function text() {
+            curl_setopt($this->curl, CURLOPT_URL, "https://api.bytestobits.dev/text");
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->headers);
+            curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_exec($this->curl);
         }
     }
 
