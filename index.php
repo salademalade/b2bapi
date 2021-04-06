c52f70b5e34181f2322f9d631bb79f6a61830cdc
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-04-06 15:14:46 +0200

    add token attribute

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         protected $token;
 
         function __constructor($token) {
+            $this->token = $token;
             $this->base_url = "https://api.bytestobits.dev/";
             $header_obj->Authorization = $token;
             $this->header = json_encode($header_obj);
