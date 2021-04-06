f5c84062b169fc128b884346fb820e106bfc4e50
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-04-06 15:31:40 +0200

    add destructor

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
         protected $token;
 
         function __constructor($token) {
+            $curl = curl_init();
             $this->token = $token;
             $this->base_url = "https://api.bytestobits.dev/";
             $header_obj->Authorization = $token;
             $this->header = json_encode($header_obj);
+        }
+        function __destructor() {
+            curl_close($curl);
         }
     }
 
