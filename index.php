c48c5e700e53faa8dd197181f66e43e8bea8f036
ichbinderwooj <75523802+ichbinderwooj@users.noreply.github.com>
2021-04-07 14:48:31 +0200

    add texts endpoint

--- index.php
+++ index.php
@@ -1,25 +1,34 @@
 <?php 
 
     class BytesToBits {
-        protected $token;
+        public $token;
 
         function __construct($token) {
-            $this->curl = curl_init();
-            $this->token = $token;
             $this->base_url = "https://api.bytestobits.dev/";
             $this->headers = array(
                 "Authorization: ".$token
             );
         }
-        function __destruct() {
-            curl_close($this->curl);
-        }
         function text() {
-            curl_setopt($this->curl, CURLOPT_URL, "https://api.bytestobits.dev/text");
-            curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->headers);
-            curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
-            $result = curl_exec($this->curl);
-            return $result;
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_URL, $this->base_url."text");
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
+            curl_setopt($curl, CURLOPT_HEADER, 0);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+            $result = curl_exec($curl);
+            if (curl_error($curl)) {
+                $err = curl_error($curl);
+                curl_close($curl);
+                return $err;
+            }
+            else {
+                curl_close($curl);
+                return $result;
+            }
+            
+            
         }
     }
 
