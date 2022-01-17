824f2a55573a8ccc5fa3f62fd8c9b7fd62531cfb
Woojin Son <75523802+ichbinderwooj@users.noreply.github.com>
2022-01-17 18:48:56 +0100

    added code

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
-# b2bapi
- 
+# PHP BytesToBits Wrapper
+
+Welcome to the repository for the PHP BytesToBits Wrapper.
+
+## Getting started
+
+To use the BytesToBits API, you need a token. You can acquire one at https://api.bytestobits.dev by making an account.
+
+## Installation
+
+Write:
+
+```
+composer require ichbinderwooj/b2bapi
+```
+
+in your command line to install this package.
+
+## Use
+
+To use this wrapper, declare a variable with the `BytesToBits` class and your token. For example:
+
+```php
+$btb = new BytesToBits($myToken);
+```
+
+Then you can use the methods of this class to access each endpoint.
+
+For example:
+
+```php
+$btb = new BytesToBits($myToken);
+echo $btb->text();
+```
+
+## Methods
+
+`word() : string`
+
+returns a random word from the BytesToBits API.
+
+`text() : string`
+
+returns a random text from the BytesToBits API.
+
+`madlibs() : array("title", "variables", "text", "questions")`
+
+returns a madlib array from the BtesToBits API.
+
+`meme() : array("title", "url", "link", "subreddit", "upvotes", "downvotes", "comments")`
+
+returns a meme from r/memes, r/dankmemes, r/me_irl or r/meirl.
+
+`reddit($subreddit : string, $limit = 0 : int) : array()`
+
+returns a list of fetched reddit posts.
+
+`lyrics($song : string, $artist : string) : array("title", "artist", "lyrics")`
+
+returns the lyrics of a song.
+
+`info() : array("uses", "limit", "next_reset")`
+
+returns information regarding your token.
---
+++ src/index.php
@@ -0,0 +1,148 @@
+<?php
+namespace Ichbinderwooj\B2bapi;
+
+class BytesToBits {
+    public $token;
+
+    function __construct($token) {
+        $this->base_url = "https://api.bytestobits.dev/";
+        $this->headers = array(
+            "Authorization: ".$token
+        );
+    }
+    function word() {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->base_url."word");
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($curl);
+        if (curl_error($curl)) {
+            $err = curl_error($curl);
+            curl_close($curl);
+            return $err;
+        }
+        else {
+            curl_close($curl);
+            return $result;
+        }
+    }
+    function text() {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->base_url."text");
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($curl);
+        if (curl_error($curl)) {
+            $err = curl_error($curl);
+            curl_close($curl);
+            return $err;
+        }
+        else {
+            curl_close($curl);
+            return $result;
+        }
+    }
+    function madlibs() {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->base_url."madlibs");
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($curl);
+        if (curl_error($curl)) {
+            $err = curl_error($curl);
+            curl_close($curl);
+            return $err;
+        }
+        else {
+            curl_close($curl);
+            return json_decode($result, true);
+        }
+    }
+    function meme() {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->base_url."meme");
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($curl);
+        if (curl_error($curl)) {
+            $err = curl_error($curl);
+            curl_close($curl);
+            return $err;
+        }
+        else {
+            curl_close($curl);
+            return json_decode($result, true);
+        }
+    }
+    function reddit($subreddit, $limit=0) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->base_url."reddit?subreddit=".$subreddit.($limit ? "&limit=".$limit : ""));
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($curl);
+        if (curl_error($curl)) {
+            $err = curl_error($curl);
+            curl_close($curl);
+            return $err;
+        }
+        else {
+            curl_close($curl);
+            return json_decode($result, true);
+        }
+    }
+    function lyrics($song, $artist) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->base_url."lyrics?song=".$song."&artist=".$artist);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($curl);
+        if (curl_error($curl)) {
+            $err = curl_error($curl);
+            curl_close($curl);
+            return $err;
+        }
+        else {
+            curl_close($curl);
+            return json_decode($result, true);
+        }
+    }
+    function info() {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->base_url."info");
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($curl);
+        if (curl_error($curl)) {
+            $err = curl_error($curl);
+            curl_close($curl);
+            return $err;
+        }
+        else {
+            curl_close($curl);
+            return json_decode($result, true);
+        }
+    }
+}
+
+?>
